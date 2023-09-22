e92372188d3f2e9fee1b8c553dd0e7909562d712
MrIgDem <140260639+MrIgDem@users.noreply.github.com>
2023-09-22 21:32:23 +0300

    Delete Лекции/Знакомство с языками программирования/Урок 1. Знакомство с языком программирования C#/Examples directory

--- Лекции/Знакомство с языками программирования/Урок 1. Знакомство с языком программирования C#/Examples/Example_002/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.WriteLine("Привет, " + username);
