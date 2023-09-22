ea05ec6c5da8bdf7fbb121942b63d9eadb1d508a
MrIgDem <analizatop69@gmail.com>
2023-09-23 01:12:02 +0300

    add2

---
+++ 002 Func_massiv/2_11 metod_v_massive/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Clear();
+
+void FillArray(int[] collection) // Метод рандомного заполнения массива
+{                                
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // Метод принта списка
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[]  collection, int find) // Метод поиска в массиве
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // Массив на 10 эл.
+
+FillArray(array); // Вызов метода FillAray 
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
