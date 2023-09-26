aafe6638bf91209ff637d7a4d10d623667a13ade
MrIgDem <analizatop69@gmail.com>
2023-09-26 22:41:56 +0300

    addGb

---
+++ Урок 2 Seminar/Урок 2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Console.Clear();
+
+// // Random rand1 = new Random();
+// // int rando = rand1.Next(10, 99);
+
+// // int rand = new Random().Next(10, 100);
+// // Console.WriteLine(rando);
+
+// // int num1 = rand / 10;
+// // int num2 = rand % 10;
+
+// //----------------------------------------------------
+
+// // int rand = new Random().Next(100, 1000);
+// // Console.WriteLine($"{rand}");
+// // int num1 = rand / 100;
+// // int num3 = rand % 100 % 10; 
+// // Console.WriteLine($"{num1}{num3}");
+
+// // //--------------------------------------------------
+// // Console.WriteLine("Введите первое число");
+// // int num1 = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Введите первое число");
+// // int num2 = Convert.ToInt32(Console.ReadLine());
+// // if(num1 * num1 == num2 || num2 * num2 == num1)
+// // {
+// //     Console.WriteLine("Да");
+// // }
+// // else
+// // {
+// //     Console.WriteLine("Нет");
+// // }
+
+// //---------------------------------------------------------------------
+
+// // Console.WriteLine("Введите первое число");
+// // int num1 = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Введите первое число");
+// // int num2 = Convert.ToInt32(Console.ReadLine());
+// // int chas = num1 % num2;
+// // if(chas == 0)
+// // {
+// //     Console.WriteLine("Кратно");
+// // }
+// // else if(chas != 0)
+// // {
+// //     Console.WriteLine($"Не кратно, остаток {chas}");
+// // }
+
+// //---------------------------------------------------------------------------
+
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// if(num1 % 7 == 0 & num1 % 23 == 0)
+// {
+//     Console.WriteLine($"Да");
+// }
+// else
+// {
+//     Console.WriteLine($"Нет");
+// }
---
+++ Урок 3 функция продолжение/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Text.Encodings.Web;
+
+Console.Clear();
+// Вид 1
+// void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+// Method1();
+
+//----------------------------------------------------------------
+
+// void Method2(string msq)
+// {
+//     Console.WriteLine(msq);
+// }
+// Method2("Текст сообщения");
+
+// void Method2_1(string msq, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msq);
+//         i++;
+//     }
+// }
+// Method2_1("Текст сообщения", 4);
+// Method2_1(msq: "Сообщение", count: 4);
+// Method2_1(count: 4, msq: "Новое");
+
+//-----------------------------------------------------------------
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+//------------------------------------------------------------------
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty;
+
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "asdf" );
+// Console.WriteLine(res);
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
++ " ежели бы вас послали вместо нашего милого Винценгероде,"
++ "вы бы взяли приступом согласие "
++ "прусского короля. Вы так красноречивы. "
++ "Вы дадите мне чаю? ";
+Console.WriteLine(text);
+
+string Replace(string text, char oldSym, char newSym)
+{
+    string result = String.Empty;
+    int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldSym) result = result + $"{newSym}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+
+} 
+
+string NewText = Replace(text, ' ', '|');
+Console.WriteLine(NewText);
+
+NewText = Replace(text, ' ', '|');
+Console.WriteLine(NewText);
