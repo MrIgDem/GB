79e00665f9c5e47973b01b1190beffa1c1ddde0c
MrIgDem <analizatop69@gmail.com>
2023-09-26 22:40:18 +0300

    add

---
+++ 002 Func_massiv/2_10 poisk_v_massive/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] mass = {11, 22, 33, 44, 55, 66, 77, 44, 88};
+int n = mass.Length;
+
+int find = 44;
+int index = 0;
+
+while(index < n)
+{
+    if(mass[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
---
+++ 002 Func_massiv/2_8 Func/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 10;
+int b1 = 12;
+int c1 = 8;
+
+int a2 = 102;
+int b2 = 1232;
+int c2 = 80;
+
+int a3 = 1011;
+int b3 = 1222;
+int c3 = 8;
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
