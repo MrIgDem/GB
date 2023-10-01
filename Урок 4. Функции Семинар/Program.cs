7bb27ac16a02a2f0e7c805370be2bcc5f0d3a4bc
MrIgDem <analizatop69@gmail.com>
2023-10-01 16:49:24 +0300

    addall

--- Урок 4. Функции Семинар/Program.cs
+++ Урок 4. Функции Семинар/Program.cs
@@ -1,4 +1,90 @@
-﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
-// 7 -> 28
-// 4 -> 10
-// 8 -> 36
+﻿Console.Clear();
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+// int count = 0;
+
+// int getNumber()
+// {
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     return number;
+// }
+
+// int countElements(int number)
+// {
+//     if(number < 0)
+//     {
+//         number = number * -1;
+//     }
+    
+//     while(number > 0)
+//     {
+//         number = number / 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// int num1 = getNumber();
+// int sum = countElements(num1);
+// Console.WriteLine(sum);
+
+
+
+// int Print(string text)
+// {
+// Console.WriteLine(text);
+// int number = Convert.ToInt32(Console.ReadLine());
+// return number;
+// }
+// static int DigitsNumber (int value){
+// if (value < 0)
+// {
+// value = -value;
+// }
+
+// string number = value.ToString();
+// return number.Length;
+// }
+// int number = Print("Введите число");
+
+// Console.WriteLine(DigitsNumber(number));
+
+//--------------------------------------------------------------
+
+// Задача № 30
+
+// int[] array = new int[8];
+
+// void FillArray(int[] array)
+// {
+//     int length = array.Length;
+//     Random random = new Random(); // Создаем объект Random один раз.
+    
+//     for (int index = 0; index < length; index++)
+//     {
+//         array[index] = random.Next(0, 2);
+//     }
+// }
+
+// FillArray(array); // Вызываем функцию для заполнения массива.
+
+// // Выводим элементы массива поочередно.
+// foreach (int element in array)
+// {
+//     Console.Write(element);
+// }
+
+//--------------------------------------------------------------------------------
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+for (int i = 0; i < num2; i++)
+{
+    count = count * num1;
+    
+}
+
+Console.WriteLine(count);
---
+++ Урок 5. Функции и одномерные массивы/Program.cs
@@ -0,0 +1,76 @@
+﻿Console.Clear();
+
+// int[] array = new int[12];
+// int sumPositive = 0;
+// int sumNegative = 0;
+
+// FillArray(array);
+
+// foreach (int number in array)
+// {
+//     if (number > 0)
+//     {
+//         sumPositive += number;
+//     }
+//     else
+//     {
+//         sumNegative += number;
+//     }
+// }
+
+// Console.WriteLine($"Сумма положительных {sumPositive} сумма отрицательных {sumNegative}");
+
+
+// void FillArray(int[] array) {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(-9, 10);
+//     }
+//     Console.WriteLine(String.Join(" ", array));
+// }
+//------------------------------------------------------------------------
+// int[] array = new int[12];
+
+// void FillArray(int[] array) {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(-9, 10);
+//     }
+//     Console.WriteLine(String.Join(" ", array));
+// }
+// FillArray(array);
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] *= -1;
+// }
+
+// Console.WriteLine(String.Join(" ", array));
+
+//---------------------------------------------------------------------------------
+int num1 = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[12];
+
+void FillArray(int[] array) {
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    Console.WriteLine(String.Join(" ", array));
+}
+FillArray(array);
+
+bool ArraySearch(int[] array, int num1)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (num1 == array[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool search = ArraySearch(array, num1);
+Console.WriteLine(search);
